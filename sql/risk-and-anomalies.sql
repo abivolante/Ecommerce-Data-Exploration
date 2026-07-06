a6d4817b8a2e8595b7a8e62bd788e7735edcba93
Abigail Volante <abigailvolante1@gmail.com>
2026-07-06 15:40:09 +0100

    Update risk-and-anomalies.sql
    fixed bug on ip address

--- sql/risk-and-anomalies.sql
+++ sql/risk-and-anomalies.sql
@@ -3,14 +3,25 @@
 -- ==========================================
 
 -- Specific IP addresses associated with the highest volume of cancelled or returned orders
-SELECT 
-  e.ip_address, 
-  COUNT(DISTINCT o.order_id) AS num_cancelled_or_returned_orders
-FROM `bigquery-public-data.thelook_ecommerce.events` AS e
-INNER JOIN `bigquery-public-data.thelook_ecommerce.orders` AS o
-  ON e.user_id = o.user_id
-WHERE o.status IN ('Cancelled', 'Returned')
-GROUP BY e.ip_address
+WITH ranked_events AS (
+  SELECT
+    o.order_id,
+    e.ip_address,
+    ROW_NUMBER() OVER (
+      PARTITION BY o.order_id
+      ORDER BY ABS(TIMESTAMP_DIFF(o.created_at, e.created_at, SECOND))
+    ) AS rank
+  FROM `bigquery-public-data.thelook_ecommerce.orders` AS o
+  INNER JOIN `bigquery-public-data.thelook_ecommerce.events` AS e
+    ON o.user_id = e.user_id
+  WHERE o.status IN ('Cancelled', 'Returned')
+)
+SELECT
+  ip_address,
+  COUNT(DISTINCT order_id) AS num_cancelled_or_returned_orders
+FROM ranked_events
+WHERE rank = 1
+GROUP BY ip_address
 ORDER BY num_cancelled_or_returned_orders DESC;
 
 -- User accounts that have placed multiple orders within less than 5 minutes of each other
